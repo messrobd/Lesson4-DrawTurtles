e087679762e396189bd1b254a4bf3487adcbdc57
Rob <robmessenger@iMac.lan>
2018-03-15 16:29:20 +0100

    refactor: draw triangles at co-ordinates

--- draw_fractal.py
+++ draw_fractal.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 
 window = turtle.Screen()
 window.setup(width=500, height=500, startx=100, starty=100)
@@ -6,24 +7,47 @@ window.bgcolor("lightgrey")
 
 def drawSolidTriangle(side, pen):
     #pen = turtle.Turtle()
+    if not pen.isdown():
+        pen.down()
 
     pen.fill(True)
     for _ in range (3):
         pen.forward(side)
         pen.left(120)
     pen.fill(False)
+    pen.up()
+
+def calcTranslations(side):
+    equilateral_internal_angle = 60#degrees
+    angle = math.radians(equilateral_internal_angle)
+
+    dx_1 = 0
+    dy_1 = 0
+
+    dx_2 = side
+    dy_2 = 0
+
+    dx_3 = -side / 2
+    dy_3 = side * math.sin(angle)
+
+    dx_translations = [dx_1, dx_2, dx_3]
+    dy_translations = [dy_1, dy_2, dy_3]
+
+    return dx_translations, dy_translations
 
 def drawFractal(side):
     pen = turtle.Turtle()
     pen.hideturtle()
 
-    left_turns = [0,0,120]
-    forward_steps = [0,side,side]
+    x_origin, y_origin = 0, 0
+    dx_translations, dy_translations = calcTranslations(side)
 
-    for turn, step in zip(left_turns, forward_steps):
-        pen.left(turn)
-        pen.forward(step)
-        pen.right(turn)
+    pen.up()
+    pen.goto(x_origin, y_origin)
+    for dx, dy in zip(dx_translations, dy_translations):
+        x_pos = pen.xcor() + dx
+        y_pos = pen.ycor() + dy
+        pen.goto(x_pos, y_pos)
         drawSolidTriangle(side, pen)
 
 
@@ -32,7 +56,7 @@ def drawFractal(side):
 
 
 
-
+#math.pow(3, order)
 
 
 
@@ -42,3 +66,11 @@ def drawFractal(side):
 drawFractal(100)
 
 window.exitonclick()
+
+#tests
+def tests():
+    side = 100
+
+    print calcDxDy(side)
+
+#tests()
